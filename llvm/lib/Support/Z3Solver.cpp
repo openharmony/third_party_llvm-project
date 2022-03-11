openharmony/third_party_llvm-project

commit a6f078465a5fc5ad2c6f06e910c5ebfbf4b1a30d

Conditional include of missing headers under Z3

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,10 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
+#include "llvm/ADT/SmalLString.h"
 #include "llvm/ADT/Twine.h"
+
+#include <set>
 
 #include <z3.h>
 

commit 7e5aa0481f0670e9377bf0c9886fb8f83d0abb21

[analyzer] Fix include typo introduced by a6f0784

llvm/lib/Support/Z3Solver.cpp
@@ -13,7 +13,7 @@ using namespace llvm;
 
 #if LLVM_WITH_Z3
 
-#include "llvm/ADT/SmalLString.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/Twine.h"
 
 #include <set>