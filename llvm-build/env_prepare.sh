e39b5787045da83f3ede002b58a492f66bf5c7f0
liwentao_uiw <liwentao20@huawei.com>
2022-07-10 21:45:59 +0800

    [BUILD] Refactor env prepare script for downloading (refer prebuilts_download.sh for OpenHarmony)
    Signed-off-by: liwentao <liwentao20@huawei.com>
    Change-Id: If175c0f449689f232624804d1957a380cfc52726

--- llvm-build/env_prepare.sh
+++ llvm-build/env_prepare.sh
@@ -1,18 +1,124 @@
-llvm_name=llvm
-cmake_name=linux-x86
-clang_name=clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04
-llvm_dir=clang/ohos/linux-x86_64
-cmake_dir=cmake
-cd prebuilts
-mkdir -p $llvm_dir
-mkdir -p $cmake_dir
+case $(uname -s) in
+    Linux)
 
-wget https://mirrors.huaweicloud.com/harmonyos/compiler/clang/10.0.1-62608/linux/llvm.tar.gz
-wget https://mirrors.huaweicloud.com/harmonyos/compiler/cmake/3.16.5/linux/cmake-linux-x86-3.16.5.tar.gz
-#wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-tar -xvf llvm.tar.gz
-tar -xvf cmake-linux-x86-3.16.5.tar.gz
-tar -xvf clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-mv $llvm_name $llvm_dir/llvm
-mv $cmake_name $cmake_dir/linux-x86
-mv $clang_name $llvm_dir/clang-10.0.1
+        host_platform=linux
+        ;;
+    Darwin)
+        host_platform=darwin
+        ;;
+    *)
+        echo "Unsupported host platform: $(uname -s)"
+        exit 1
+esac
+
+case $(uname -m) in
+    arm64)
+
+        host_cpu=arm64
+        ;;
+    *)
+        host_cpu=x86_64
+esac
+
+# sync code directory
+code_dir=$(pwd)
+
+# downloaded files will be automatically unzipped under this path
+bin_dir=${code_dir}/download_packages
+
+# download and initialize prebuild files
+if [ ! -d "${bin_dir}" ];then
+    mkdir -p "${bin_dir}"
+fi
+
+
+function download_and_archive() {
+    archive_dir=$1
+    download_source_url=$2
+    bin_file=$(basename ${download_source_url})
+    #wget -t3 -T10 -O "${bin_dir}/${bin_file}" "${download_source_url}"
+    if [ ! -d "${code_dir}/${archive_dir}" ];then
+        mkdir -p "${code_dir}/${archive_dir}"
+    fi
+    if [ "X${bin_file:0-3}" = "Xzip" ];then
+        unzip -o "${bin_dir}/${bin_file}" -d "${code_dir}/${archive_dir}/"
+    elif [ "X${bin_file:0-6}" = "Xtar.gz" ];then
+        tar -xvzf "${bin_dir}/${bin_file}" -C "${code_dir}/${archive_dir}"
+    else
+        tar -xvf "${bin_dir}/${bin_file}" -C "${code_dir}/${archive_dir}"
+    fi
+}
+
+
+copy_config="""
+prebuilts/clang/ohos/${host_platform}-${host_cpu},https://mirrors.huaweicloud.com/openharmony/compiler/clang/12.0.1-530132/${host_platform}/clang-530132-${host_platform}-x86_64.tar.bz2
+"""
+
+copy_config_linux_x86_64="""
+prebuilts/cmake,https://mirrors.huaweicloud.com/harmonyos/compiler/cmake/3.16.5/${host_platform}/cmake-${host_platform}-x86-3.16.5.tar.gz
+prebuilts/clang/ohos/${host_platform}-${host_cpu},https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+"""
+
+copy_config_darwin_x86_64="""
+prebuilts/cmake,https://mirrors.huaweicloud.com/harmonyos/compiler/cmake/3.16.5/${host_platform}/cmake-${host_platform}-x86-3.16.5.tar.gz
+prebuilts/clang/ohos/${host_platform}-${host_cpu},https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/clang+llvm-10.0.1-x86_64-apple-darwin.tar.xz
+"""
+
+# 
+if [[ "${host_platform}" == "linux" ]]; then
+    if [[ "${host_cpu}" == "x86_64" ]]; then
+        copy_config+=${copy_config_linux_x86_64}
+        echo "add ubuntu here"
+    else
+        echo "unknwon host_cpu - ${host_cpu} for linux"
+    fi
+elif [[ "${host_platform}" == "darwin" ]]; then
+    if [[ "${host_cpu}" == "x86_64" ]]; then
+        copy_config+=${copy_config_darwin_x86_64}
+        echo "add x86-64 mac here"
+    elif [[ "${host_cpu}" == "arm64" ]]; then
+        echo "add m1 config here"
+    else
+        echo "unknwon host_cpu - ${host_cpu} for darwin"
+    fi
+else
+    echo "unknown ${host_platform}"
+fi
+
+
+
+for i in $(echo ${copy_config})
+do
+    unzip_dir=$(echo $i|awk -F ',' '{print $1}')
+    remote_url=$(echo $i|awk -F ',' '{print $2}')
+    download_and_archive "${unzip_dir}" "${remote_url}"
+done
+
+
+if [ -d "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang-530132" ];then
+    rm -rf "${code_dir}/prebuilts/clang/ohos/linux-x86_64/llvm"
+    mv "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang-530132" "${code_dir}/prebuilts/clang/ohos/linux-x86_64/llvm"
+    ln -snf 12.0.1 "${code_dir}/prebuilts/clang/ohos/linux-x86_64/llvm/lib/clang/current"
+fi
+
+if [ -d "${code_dir}/prebuilts/clang/ohos/darwin-${host_cpu}/clang-530132" ];then
+    if [[ "${host_cpu}" == "arm64" ]]; then
+        rm -rf "${code_dir}/prebuilts/clang/ohos/darwin-arm64/llvm"
+        mv "${code_dir}/prebuilts/clang/ohos/darwin-arm64/clang-530132" "${code_dir}/prebuilts/clang/ohos/darwin-arm64/llvm"
+        ln -snf 12.0.1 "${code_dir}/prebuilts/clang/ohos/darwin-arm64/llvm/lib/clang/current"
+    else
+        rm -rf "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/llvm"
+        mv "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/clang-530132" "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/llvm"
+        ln -snf 12.0.1 "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/llvm/lib/clang/current"
+    fi
+fi
+
+if [ -d "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang+llvm-10.0.1-x86_64-gnu-ubuntu-16.04.tar.xz" ];then
+    rm -rf "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang-10.0.1"
+    mv "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang+llvm-10.0.1-x86_64-gnu-ubuntu-16.04" "${code_dir}/prebuilts/clang/ohos/linux-x86_64/clang-10.0.1"
+fi
+
+if [ -d "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/clang+llvm-10.0.1-x86_64-apple-darwin" ];then
+    rm -rf "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/clang-10.0.1"
+    mv "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/clang+llvm-10.0.1-x86_64-apple-darwin" "${code_dir}/prebuilts/clang/ohos/darwin-x86_64/clang-10.0.1"
+fi
