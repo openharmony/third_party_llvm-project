openharmony/third_party_llvm-project

commit 59d27ec43fb6fccc3a097645c7453de67a0c6282

[PERF2BOLT] Do not issue parsing error on weird build ids
In weird entries we were issueing a parse error. For example, in line 5 here:

6862acc063b0aa86595f52ff81628577df4296ff a.so
6862acc063b0aa86595f52ff81628577df4296ff a.so
6862acc063b0aa86595f52ff81628577df4296ff a.so
db758cb3c970044e78d5a4c99b011708a9995636 bin1
60326683eab31acfd03435d9ed4ff9a8         bin2
7d448e51851b4bdb33eac84f90e74628a14a5f00 b.so
742aa26e0211794356cc25f415c25230a26aa045 c.so

Error reading BOLT data input file: line 89, column 33: malformed field

Fix that.

Reviewed By: #bolt, Amir

Differential Revision: https://reviews.llvm.org/D134822

bolt/lib/Profile/DataAggregator.cpp
@@ -2147,7 +2147,8 @@ DataAggregator::parseNameBuildIDPair() {
 
   // If one of the strings is missing, don't issue a parsing error, but still
   // do not return a value.
-  if (ParsingBuf[0] == '\n')
+  consumeAllRemainingFS();
+  if (checkAndConsumeNewLine())
     return NoneType();
 
   ErrorOr<StringRef> NameStr = parseString(FieldSeparator, true);

commit 88c29f0ab9b765a7c5f570abfb6331a0608782c5

[PERF2BOLT] Fix unittest failure
Fix failure caused by commit 59d27ec43f "Do not issue parsing error on
weird build ids".

bolt/include/bolt/Profile/DataAggregator.h
@@ -311,6 +311,10 @@ private:
   /// Consume the entire line
   void consumeRestOfLine();
 
+  /// True if the next token in the parsing buffer is a new line, but don't
+  /// consume it (peek only).
+  bool checkNewLine();
+
   /// Parse a single LBR entry as output by perf script -Fbrstack
   ErrorOr<LBREntry> parseLBREntry();
 

bolt/lib/Profile/DataAggregator.cpp
@@ -1056,6 +1056,10 @@ void DataAggregator::consumeRestOfLine() {
   Line += 1;
 }
 
+bool DataAggregator::checkNewLine() {
+  return ParsingBuf[0] == '\n';
+}
+
 ErrorOr<DataAggregator::PerfBranchSample> DataAggregator::parseBranchSample() {
   PerfBranchSample Res;
 
@@ -2148,7 +2152,7 @@ DataAggregator::parseNameBuildIDPair() {
   // If one of the strings is missing, don't issue a parsing error, but still
   // do not return a value.
   consumeAllRemainingFS();
-  if (checkAndConsumeNewLine())
+  if (checkNewLine())
     return NoneType();
 
   ErrorOr<StringRef> NameStr = parseString(FieldSeparator, true);