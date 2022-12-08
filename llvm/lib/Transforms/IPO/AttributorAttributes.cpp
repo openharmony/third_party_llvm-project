openharmony/third_party_llvm-project

commit 2933ed3942cdc2ddf8f9c1169dfac688963958a4

Fix an unused-variable warning in release build, NFC

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,8 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
-        assert(IsDead && "Store was assumed to be dead!");
+        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
+               "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];

commit d7c5a7bb481258079890358d153651cbdb12500f

[Attributor] Fix a break in relase build caused by 2933ed3942cdc2ddf8f9c1169dfac688963958a4.

llvm/lib/Transforms/IPO/AttributorAttributes.cpp
@@ -3722,8 +3722,9 @@ struct AAIsDeadFloating : public AAIsDeadValueImpl {
       // still needed.
       if (auto *SI = dyn_cast<StoreInst>(I)) {
         SmallSetVector<Instruction *, 8> AssumeOnlyInst;
-        assert(isDeadStore(A, *SI, &AssumeOnlyInst) &&
-               "Store was assumed to be dead!");
+        bool IsDead = isDeadStore(A, *SI, &AssumeOnlyInst);
+        (void)IsDead;
+        assert(IsDead && "Store was assumed to be dead!");
         A.deleteAfterManifest(*I);
         for (size_t i = 0; i < AssumeOnlyInst.size(); ++i) {
           Instruction *AOI = AssumeOnlyInst[i];