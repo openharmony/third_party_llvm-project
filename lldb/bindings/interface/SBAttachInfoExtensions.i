openharmony/third_party_llvm-project

commit 1d485bcad5c3d4467cf4070f1d2a2dea1707372a

Re-land "[lldb/docs] Silence warnings when generating website"
This reverts commit 5dc7b6a1a8d2b2420ea6a248e5f856593e1f66ba and fix the
build failure issues introduced because of the `STRING_EXTENSION_OUTSIDE`
swig macros.

Differential Revision: https://reviews.llvm.org/D159017

Signed-off-by: Med Ismail Bennani <ismail@bennani.ma>

lldb/bindings/interface/SBAttachInfoExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBAttachInfo)
+
+%extend lldb::SBAttachInfo {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBBreakpointListExtensions.i
@@ -1,0 +1,29 @@
+STRING_EXTENSION_OUTSIDE(SBBreakpointList)
+
+%extend lldb::SBBreakpointList {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+        return not self.__ne__(other)
+
+    def __int__(self):
+        pass
+
+    def __len__(self):
+        '''Return the number of breakpoints in a lldb.SBBreakpointList object.'''
+        return self.GetSize()
+
+    def __hex__(self):
+        pass
+
+    def __oct__(self):
+        pass
+
+    def __iter__(self):
+        '''Iterate over all breakpoints in a lldb.SBBreakpointList object.'''
+        return lldb_iter(self, 'GetSize', 'GetBreakpointAtIndex')
+    %}
+#endif
+}

lldb/bindings/interface/SBBroadcastExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBBroadcaster)
+
+%extend lldb::SBBroadcaster {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBCommandInterpreterExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBCommandInterpreter)
+
+%extend lldb::SBCommandInterpreter {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBCommandInterpreterRunOptionsExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBCommandInterpreterRunOptions)
+
+%extend lldb::SBCommandInterpreterRunOptions {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBCommunicationExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBCommunication)
+
+%extend lldb::SBCommunication {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBEnvironmentExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBEnvironment)
+
+%extend lldb::SBEnvironment {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

lldb/bindings/interface/SBExpressionOptionsExtensions.i
@@ -1,0 +1,27 @@
+STRING_EXTENSION_OUTSIDE(SBExpressionOptions)
+
+%extend lldb::SBExpressionOptions {
+#ifdef SWIGPYTHON
+    // operator== is a free function, which swig does not handle, so we inject
+    // our own equality operator here
+    %pythoncode%{
+    def __eq__(self, other):
+      return not self.__ne__(other)
+
+    def __int__(self):
+      pass
+
+    def __hex__(self):
+      pass
+
+    def __oct__(self):
+      pass
+
+    def __len__(self):
+      pass
+
+    def __iter__(self):
+      pass
+    %}
+#endif
+}

commit 7b48775382d652ed48855ff9c8aa9fdcab44e02a

[lldb] Fix build failures introduced in 1d485bcad5c3 (NFCI)
This patch should fix the build failures introduced by 1d485bcad5c3.

This removes the use of the `STRING_EXTENSION_OUTSIDE` swig macro in SB
classes that don't implement a `GetDescription` method.

Signed-off-by: Med Ismail Bennani <ismail@bennani.ma>

lldb/bindings/interface/SBAttachInfoExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBAttachInfo)
-
 %extend lldb::SBAttachInfo {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBBreakpointListExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBBreakpointList)
-
 %extend lldb::SBBreakpointList {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBBroadcastExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBBroadcaster)
-
 %extend lldb::SBBroadcaster {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBCommandInterpreterExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBCommandInterpreter)
-
 %extend lldb::SBCommandInterpreter {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBCommandInterpreterRunOptionsExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBCommandInterpreterRunOptions)
-
 %extend lldb::SBCommandInterpreterRunOptions {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBCommunicationExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBCommunication)
-
 %extend lldb::SBCommunication {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBEnvironmentExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBEnvironment)
-
 %extend lldb::SBEnvironment {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBExpressionOptionsExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBExpressionOptions)
-
 %extend lldb::SBExpressionOptions {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBHostOSExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBHostOS)
-
 %extend lldb::SBHostOS {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBLanguageRuntimeExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBLanguageRuntime)
-
 %extend lldb::SBLanguageRuntime {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBLaunchInfoExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBLaunchInfo)
-
 %extend lldb::SBLaunchInfo {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBListenerExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBListener)
-
 %extend lldb::SBListener {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBMemoryRegionInfoListExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBMemoryRegionInfoList)
-
 %extend lldb::SBMemoryRegionInfoList {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBPlatformConnectOptionsExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBPlatformConnectOptions)
-
 %extend lldb::SBPlatformConnectOptions {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBPlatformExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBPlatform)
-
 %extend lldb::SBPlatform {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBPlatformShellCommandExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBPlatformShellCommand)
-
 %extend lldb::SBPlatformShellCommand {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBProcessInfoExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBProcessInfo)
-
 %extend lldb::SBProcessInfo {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBQueueExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBQueue)
-
 %extend lldb::SBQueue {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBQueueItemExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBQueueItem)
-
 %extend lldb::SBQueueItem {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBReproducerExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBReproducer)
-
 %extend lldb::SBReproducer {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBScriptObjectExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBScriptObject)
-
 %extend lldb::SBScriptObject {
 #ifdef SWIGPYTHON
     %pythoncode %{

lldb/bindings/interface/SBSourceManagerExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBSourceManager)
-
 %extend lldb::SBSourceManager {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBThreadCollectionExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBThreadCollection)
-
 %extend lldb::SBThreadCollection {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBTraceCursorExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBTraceCursor)
-
 %extend lldb::SBTraceCursor {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBTraceExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBTrace)
-
 %extend lldb::SBTrace {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBTypeSummaryOptionsExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBTypeSummaryOptions)
-
 %extend lldb::SBTypeSummaryOptions {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject

lldb/bindings/interface/SBVariablesOptionsExtensions.i
@@ -1,5 +1,3 @@
-STRING_EXTENSION_OUTSIDE(SBVariablesOptions)
-
 %extend lldb::SBVariablesOptions {
 #ifdef SWIGPYTHON
     // operator== is a free function, which swig does not handle, so we inject