openharmony/third_party_llvm-project

commit 5629777cb92f27176a90ca4545790795230b1e76

[clang-format] Rename qualifier file comments (#64354)
* [clang-format] rename the file comments to match the file name

* [clang-format] rename the file comments to match the file name

* Remove extraneous space

* [clang-format] NFC remove EOF

---------

Co-authored-by: paul_hoad <paul_hoad@amat.com>

clang/lib/Format/QualifierAlignmentFixer.cpp
@@ -1,4 +1,4 @@
-//===--- LeftRightQualifierAlignmentFixer.cpp -------------------*- C++--*-===//
+//===--- QualifierAlignmentFixer.cpp ----------------------------*- C++--*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -7,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file implements LeftRightQualifierAlignmentFixer, a TokenAnalyzer that
+/// This file implements QualifierAlignmentFixer, a TokenAnalyzer that
 /// enforces either left or right const depending on the style.
 ///
 //===----------------------------------------------------------------------===//

clang/lib/Format/QualifierAlignmentFixer.h
@@ -1,5 +1,4 @@
-//===--- LeftRightQualifierAlignmentFixer.h ------------------------------*- C++
-//-*-===//
+//===--- QualifierAlignmentFixer.h -------------------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -8,7 +7,7 @@
 //===----------------------------------------------------------------------===//
 ///
 /// \file
-/// This file declares LeftRightQualifierAlignmentFixer, a TokenAnalyzer that
+/// This file declares QualifierAlignmentFixer, a TokenAnalyzer that
 /// enforces either east or west const depending on the style.
 ///
 //===----------------------------------------------------------------------===//

commit 3b6d4e235d3b8499c3092dda5a9e51a230a6eca3

[clang-format][NFC] Reformat and fix file mode
Fix file mode errors introduced in 5629777cb92f and format errors in
https://reviews.llvm.org/D155239.

clang/include/clang/Format/Format.h
@@ -4335,17 +4335,15 @@ struct FormatStyle {
           InEmptyParentheses(false), Other(false) {}
 
     SpacesInParensCustom(bool InConditionalStatements, bool InCStyleCasts,
-        bool InEmptyParentheses, bool Other)
+                         bool InEmptyParentheses, bool Other)
         : InConditionalStatements(InConditionalStatements),
-          InCStyleCasts(InCStyleCasts),
-          InEmptyParentheses(InEmptyParentheses),
+          InCStyleCasts(InCStyleCasts), InEmptyParentheses(InEmptyParentheses),
           Other(Other) {}
 
     bool operator==(const SpacesInParensCustom &R) const {
       return InConditionalStatements == R.InConditionalStatements &&
              InCStyleCasts == R.InCStyleCasts &&
-             InEmptyParentheses == R.InEmptyParentheses &&
-             Other == R.Other;
+             InEmptyParentheses == R.InEmptyParentheses && Other == R.Other;
     }
     bool operator!=(const SpacesInParensCustom &R) const {
       return !(*this == R);