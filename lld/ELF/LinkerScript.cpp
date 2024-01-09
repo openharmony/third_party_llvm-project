openharmony/third_party_llvm-project

commit aa7147df93d611e883f000fd509ca785e90bdbc0

[ELF] -r: rename orphan SHT_REL/SHT_RELA when the relocated input section is placed in an output section
This ports https://reviews.llvm.org/D40652 (--emit-relocs) to -r and
matches GNU ld.
Close #67910

lld/ELF/LinkerScript.cpp
@@ -51,11 +51,8 @@ static bool isSectionPrefix(StringRef prefix, StringRef name) {
 }
 
 static StringRef getOutputSectionName(const InputSectionBase *s) {
-  if (config->relocatable)
-    return s->name;
-
-  // This is for --emit-relocs. If .text.foo is emitted as .text.bar, we want
-  // to emit .rela.text.foo as .rela.text.bar for consistency (this is not
+  // This is for --emit-relocs and -r. If .text.foo is emitted as .text.bar, we
+  // want to emit .rela.text.foo as .rela.text.bar for consistency (this is not
   // technically required, but not doing it is odd). This code guarantees that.
   if (auto *isec = dyn_cast<InputSection>(s)) {
     if (InputSectionBase *rel = isec->getRelocatedSection()) {
@@ -65,6 +62,9 @@ static StringRef getOutputSectionName(const InputSectionBase *s) {
       return saver().save(".rel" + out->name);
     }
   }
+
+  if (config->relocatable)
+    return s->name;
 
   // A BssSection created for a common symbol is identified as "COMMON" in
   // linker scripts. It should go to .bss section.

lld/test/ELF/linkerscript/emit-reloc-section-names.s
@@ -3,6 +3,8 @@
 # RUN: echo "SECTIONS { .text.zed : { *(.text.foo) } .text.qux : { *(.text.bar) } }" > %t.script
 # RUN: ld.lld -T %t.script --emit-relocs %t.o -o %t
 # RUN: llvm-objdump --section-headers %t | FileCheck %s
+# RUN: ld.lld -T %t.script -r %t.o -o %t.ro
+# RUN: llvm-readelf -S %t.ro | FileCheck %s
 
 ## Check we name relocation sections in according to
 ## their target sections names.

lld/test/ELF/linkerscript/emit-relocs-multiple.s
@@ -3,6 +3,8 @@
 # RUN: echo "SECTIONS { .zed : { *(.foo) *(.bar) } }" > %t.script
 # RUN: ld.lld --emit-relocs --script %t.script %t.o -o %t1
 # RUN: llvm-readobj -r %t1 | FileCheck %s
+# RUN: ld.lld -r --script %t.script %t.o -o %t.ro
+# RUN: llvm-readobj -r %t.ro | FileCheck %s
 
 # CHECK:      Relocations [
 # CHECK-NEXT:   Section {{.*}} .rela.zed {

commit 40b0f9a2a43e6fafa51c27699529a23eebab159f

[ELF] -r: fix crash when SHF_LINK_ORDER linked-to section has a larger index
Fixes: aa7147df93d611e883f000fd509ca785e90bdbc0

ThinLTO asan build may place `asan_globals` before the associated `.bss.xxx` section.
`rel->getOutputSection()` is nullptr because `rel->parent` hasn't been
set, leading to a crash. Simplify return `s->name` in this case.

lld/ELF/LinkerScript.cpp
@@ -57,6 +57,10 @@ static StringRef getOutputSectionName(const InputSectionBase *s) {
   if (auto *isec = dyn_cast<InputSection>(s)) {
     if (InputSectionBase *rel = isec->getRelocatedSection()) {
       OutputSection *out = rel->getOutputSection();
+      if (!out) {
+        assert(config->relocatable && (rel->flags & SHF_LINK_ORDER));
+        return s->name;
+      }
       if (s->type == SHT_RELA)
         return saver().save(".rela" + out->name);
       return saver().save(".rel" + out->name);

lld/test/ELF/linkorder-group.test
@@ -1,0 +1,58 @@
+# REQUIRES: x86
+## Test SHF_LINK_ORDER when the linked-to section has a larger index.
+# RUN: yaml2obj %s -o %t.o
+# RUN: ld.lld -r %t.o -o %t.ro
+# RUN: llvm-readelf -x asan_globals %t.ro | FileCheck %s
+
+# CHECK:      Hex dump of section 'asan_globals':
+# CHECK-NEXT: 0x00000000 00                                  .
+
+--- !ELF
+FileHeader:
+  Class:           ELFCLASS64
+  Data:            ELFDATA2LSB
+  Type:            ET_REL
+  Machine:         EM_X86_64
+  SectionHeaderStringTable: .strtab
+Sections:
+  - Name:            .text
+    Type:            SHT_PROGBITS
+    Flags:           [ SHF_ALLOC, SHF_EXECINSTR ]
+  - Name:            .bss
+    Type:            SHT_NOBITS
+    Flags:           [ SHF_WRITE, SHF_ALLOC, SHF_GROUP ]
+    Size:            0x1
+  - Name:            asan_globals
+    Type:            SHT_PROGBITS
+    Flags:           [ SHF_ALLOC, SHF_LINK_ORDER, SHF_GROUP ]
+    Link:            .bss
+    Content:         '00'
+  - Name:            .group
+    Type:            SHT_GROUP
+    Link:            .symtab
+    Info:            foo
+    Members:
+      - SectionOrType:   GRP_COMDAT
+      - SectionOrType:   .bss
+      - SectionOrType:   asan_globals
+      - SectionOrType:   .relaasan_globals
+  - Name:            .relaasan_globals
+    Type:            SHT_RELA
+    Flags:           [ SHF_GROUP ]
+    Link:            .symtab
+    Info:            asan_globals
+    Relocations:
+      - Type:            R_X86_64_NONE
+  - Type:            SectionHeaderTable
+    Sections:
+      - Name:            .strtab
+      - Name:            .text
+      - Name:            .group
+      - Name:            asan_globals
+      - Name:            .relaasan_globals
+      - Name:            .bss
+      - Name:            .symtab
+Symbols:
+  - Name:            foo
+    Section:         .bss
+    Binding:         STB_WEAK