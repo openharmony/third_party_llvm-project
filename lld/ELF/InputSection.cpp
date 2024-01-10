c5c99982e9657ae9bc0daaad8cbb666a6dd0283f
Lu Weining <luweining@loongson.cn>
2024-01-10 18:03:52 +0800

    [lld][LoongArch] Handle extreme code model relocs according to psABI v2.30 (#73387)
    psABI v2.30 requires the extreme code model instructions sequence
    (pcalau12i+addi.d+lu32i.d+lu52i.d) to be adjacent.
    
    See https://github.com/llvm/llvm-project/pull/71907 and
    https://github.com/loongson-community/discussions/issues/17 for details.
    
    (cherry picked from commit 38394a3d0b8b9a1fdc444bdebeba17a19250997d)
    Change-Id: I6ece8fdb9f9c7ac2baf086153e5b71865d93fd76
    (cherry picked from commit 2a30bd5bf5ce238b5677943dd93d26c5c3ebc08d)
    Signed-off-by: Ami-zhang <zhanglimin@loongson.cn>

--- lld/ELF/InputSection.cpp
+++ lld/ELF/InputSection.cpp
@@ -668,8 +668,8 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
     return sym.getGotVA() + a - p;
   case R_LOONGARCH_GOT_PAGE_PC:
     if (sym.needsTlsGd)
-      return getLoongArchPageDelta(in.got->getGlobalDynAddr(sym) + a, p);
-    return getLoongArchPageDelta(sym.getGotVA() + a, p);
+      return getLoongArchPageDelta(in.got->getGlobalDynAddr(sym) + a, p, type);
+    return getLoongArchPageDelta(sym.getGotVA() + a, p, type);
   case R_MIPS_GOTREL:
     return sym.getVA(a) - in.mipsGot->getGp(file);
   case R_MIPS_GOT_GP:
@@ -719,7 +719,7 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
     return 0;
   }
   case R_LOONGARCH_PAGE_PC:
-    return getLoongArchPageDelta(sym.getVA(a), p);
+    return getLoongArchPageDelta(sym.getVA(a), p, type);
   case R_PC:
   case R_ARM_PCA: {
     uint64_t dest;
@@ -754,7 +754,7 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
   case R_PPC64_CALL_PLT:
     return sym.getPltVA() + a - p;
   case R_LOONGARCH_PLT_PAGE_PC:
-    return getLoongArchPageDelta(sym.getPltVA() + a, p);
+    return getLoongArchPageDelta(sym.getPltVA() + a, p, type);
   case R_PLT_GOTPLT:
     return sym.getPltVA() + a - in.gotPlt->getVA();
   case R_PPC32_PLTREL:
@@ -816,7 +816,7 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
   case R_TLSGD_PC:
     return in.got->getGlobalDynAddr(sym) + a - p;
   case R_LOONGARCH_TLSGD_PAGE_PC:
-    return getLoongArchPageDelta(in.got->getGlobalDynAddr(sym) + a, p);
+    return getLoongArchPageDelta(in.got->getGlobalDynAddr(sym) + a, p, type);
   case R_TLSLD_GOTPLT:
     return in.got->getVA() + in.got->getTlsIndexOff() + a - in.gotPlt->getVA();
   case R_TLSLD_GOT:
