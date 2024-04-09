c7c55fa728c38f959ce954b96518978a20595670
Lyupa Anastasia <lyupa.anastasia@huawei.com>
2024-04-09 12:33:31 +0300

    [compiler-rt][OHOS] Remove print in hdc_constants
    Issue: https://gitee.com/openharmony/third_party_llvm-project/issues/I9FXQY
    
    Signed-off-by: Lyupa Anastasia <lyupa.anastasia@huawei.com>

--- compiler-rt/test/sanitizer_common/ohos_family_commands/hdc_constants.py
+++ compiler-rt/test/sanitizer_common/ohos_family_commands/hdc_constants.py
@@ -3,15 +3,12 @@ import os
 
 # TODO: move this to the cmake ?
 HDC = os.environ.get('HDC', 'hdc')
+# Please set "HDC_SERVER_IP_PORT" and "HDC_UTID" environment variables
+# to pass options for connecting to remote device if needed
 HDC_SERVER_IP_PORT = os.environ.get('HDC_SERVER_IP_PORT')
 HDC_UTID = os.environ.get('HDC_UTID')
 TMPDIR = os.environ.get('OHOS_REMOTE_TMP_DIR', '/data/local/tmp/Output')
 DYN_LINKER = os.environ.get('OHOS_REMOTE_DYN_LINKER')
-
-# emit warning on import if some required constants are not set
-if not HDC_SERVER_IP_PORT or not HDC_UTID:
-    print('Please set "HDC_SERVER_IP_PORT" and "HDC_UTID" environment variables '
-          'to be able to debug on remote device')
 
 def get_hdc_cmd_prefix():
     server = ['-s', HDC_SERVER_IP_PORT] if HDC_SERVER_IP_PORT else []
