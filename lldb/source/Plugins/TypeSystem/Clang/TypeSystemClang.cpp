openharmony/third_party_llvm-project

commit fd65a8229d0e9db8e9754dbbcc4d0d080f583843

[lldb][TypeSystemClang] Fix mismerge in GetCompleteRecordType
Mismerge committed in `27bff28203e5e81a1cc0bf02721325353db5b3de`

lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -2684,7 +2684,7 @@ static const clang::RecordType *GetCompleteRecordType(clang::ASTContext *ast,
   const auto *tag_type = llvm::cast<clang::RecordType>(qual_type.getTypePtr());
 
   clang::CXXRecordDecl *cxx_record_decl = qual_type->getAsCXXRecordDecl();
-  if (TypeSystemClang::UseRedeclCompletion()) {
+  if (TypeSystemClang::UseRedeclCompletion() && cxx_record_decl) {
     clang::CXXRecordDecl *def = cxx_record_decl->getDefinition();
     if (!def)
       return nullptr;

commit 870c8996211d8075dce254c7aafadd87702ebb45

[lldb][TypeSystemClang] Fix build failure
Fixes build failure introduced in
`fd65a8229d0e9db8e9754dbbcc4d0d080f583843`.

lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.cpp
@@ -2676,9 +2676,9 @@ TypeSystemClang::GetDeclContextForType(clang::QualType type) {
 /// function will try to complete the type if necessary (and allowed
 /// by the specified \ref allow_completion). If we fail to return a *complete*
 /// type, returns nullptr.
-static const clang::RecordType *GetCompleteRecordType(clang::ASTContext *ast,
-                                                      clang::QualType qual_type,
-                                                      bool allow_completion) {
+static const clang::Type *GetCompleteRecordType(clang::ASTContext *ast,
+                                                clang::QualType qual_type,
+                                                bool allow_completion) {
   assert(qual_type->isRecordType());
 
   const auto *tag_type = llvm::cast<clang::RecordType>(qual_type.getTypePtr());
@@ -2728,9 +2728,9 @@ static const clang::RecordType *GetCompleteRecordType(clang::ASTContext *ast,
 /// function will try to complete the type if necessary (and allowed
 /// by the specified \ref allow_completion). If we fail to return a *complete*
 /// type, returns nullptr.
-static const clang::EnumType *GetCompleteEnumType(clang::ASTContext *ast,
-                                                  clang::QualType qual_type,
-                                                  bool allow_completion) {
+static const clang::Type *GetCompleteEnumType(clang::ASTContext *ast,
+                                              clang::QualType qual_type,
+                                              bool allow_completion) {
   assert(qual_type->isEnumeralType());
   assert(ast);
 
@@ -2771,7 +2771,7 @@ static const clang::EnumType *GetCompleteEnumType(clang::ASTContext *ast,
 /// function will try to complete the type if necessary (and allowed
 /// by the specified \ref allow_completion). If we fail to return a *complete*
 /// type, returns nullptr.
-static const clang::ObjCObjectType *
+static const clang::Type *
 GetCompleteObjCObjectType(clang::ASTContext *ast, QualType qual_type,
                           bool allow_completion) {
   assert(qual_type->isObjCObjectType());