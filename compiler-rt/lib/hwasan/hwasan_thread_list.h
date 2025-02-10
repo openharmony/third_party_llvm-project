8c43a8c9836657a3780717d0b3c4f7e36488f95d
jay0528 <chencheng302@huawei.com>
2025-02-10 14:54:07 +0800

    [Compiler-RT][HWASAN] Add Format When Printing UAF in Freed Thread
    Add Format When Printing UAF in Freed Thread
    
    Signed-off-by: jay0528 <chencheng302@huawei.com>

--- compiler-rt/lib/hwasan/hwasan_thread_list.h
+++ compiler-rt/lib/hwasan/hwasan_thread_list.h
@@ -146,6 +146,14 @@ class SANITIZER_MUTEX HwasanThreadList {
       return;
 
     SpinMutexLock l(&freed_rb_mutex_);
+
+    freed_rb_count_++;
+    if (freed_rb_count_ == 0)
+      freed_rb_count_overflow_++;
+
+    if (!flags()->freed_threads_history_size)
+      return;
+
     if (!freed_rb_list_) {
       size_t sz = flags()->freed_threads_history_size *
                   sizeof(HeapAllocationsRingBuffer *);
@@ -157,6 +165,8 @@ class SANITIZER_MUTEX HwasanThreadList {
     }
     if (freed_rb_list_size_ >= flags()->freed_threads_history_size) {
       auto sz = flags()->freed_threads_history_size / 3;
+      if (sz == 0)
+        sz = 1;
       for (uptr i = 0; i < sz; i++) {
         if (freed_rb_list_[i])
           freed_rb_list_[i]->Delete();
@@ -180,9 +190,6 @@ class SANITIZER_MUTEX HwasanThreadList {
     }
     freed_rb_list_[freed_rb_list_size_] = freed_allocations_;
     freed_rb_list_size_++;
-    freed_rb_count_++;
-    if (freed_rb_count_ == 0)
-      freed_rb_count_overflow_++;
   }
 // OHOS_LOCAL end
 
