NanQin555/llvm-project

[LoongArch] Add definition for LASX instructions
This patch adds the definition of the LASX instructions, providing
support only for assembly and disassembly, similar to D154183.

Depends on D154183

Reviewed By: SixWeining

Differential Revision: https://reviews.llvm.org/D154195

llvm/lib/Target/LoongArch/MCTargetDesc/LoongArchMCCodeEmitter.cpp
@@ -279,14 +279,18 @@ void LoongArchMCCodeEmitter::expandToVectorLDI(
   int64_t Imm = MI.getOperand(1).getImm() & 0x3FF;
   switch (MI.getOpcode()) {
   case LoongArch::PseudoVREPLI_B:
+  case LoongArch::PseudoXVREPLI_B:
     break;
   case LoongArch::PseudoVREPLI_H:
+  case LoongArch::PseudoXVREPLI_H:
     Imm |= 0x400;
     break;
   case LoongArch::PseudoVREPLI_W:
+  case LoongArch::PseudoXVREPLI_W:
     Imm |= 0x800;
     break;
   case LoongArch::PseudoVREPLI_D:
+  case LoongArch::PseudoXVREPLI_D:
     Imm |= 0xC00;
     break;
   }
@@ -310,6 +314,11 @@ void LoongArchMCCodeEmitter::encodeInstruction(
   case LoongArch::PseudoVREPLI_W:
   case LoongArch::PseudoVREPLI_D:
     return expandToVectorLDI<LoongArch::VLDI>(MI, CB, Fixups, STI);
+  case LoongArch::PseudoXVREPLI_B:
+  case LoongArch::PseudoXVREPLI_H:
+  case LoongArch::PseudoXVREPLI_W:
+  case LoongArch::PseudoXVREPLI_D:
+    return expandToVectorLDI<LoongArch::XVLDI>(MI, CB, Fixups, STI);
   }
 
   switch (Size) {

openharmony/third_party_llvm-project

(cherry picked from commit f3d18b9dc6f73755b58df5f06fcae992322aeaed)
Change-Id: I83e7a521fbc45c190da23ad5503cc0c775451f7a
Signed-off-by: Ami-zhang <zhanglimin@loongson.cn>

llvm/lib/Target/LoongArch/MCTargetDesc/LoongArchMCCodeEmitter.cpp
@@ -279,14 +279,18 @@ void LoongArchMCCodeEmitter::expandToVectorLDI(
   int64_t Imm = MI.getOperand(1).getImm() & 0x3FF;
   switch (MI.getOpcode()) {
   case LoongArch::PseudoVREPLI_B:
+  case LoongArch::PseudoXVREPLI_B:
     break;
   case LoongArch::PseudoVREPLI_H:
+  case LoongArch::PseudoXVREPLI_H:
     Imm |= 0x400;
     break;
   case LoongArch::PseudoVREPLI_W:
+  case LoongArch::PseudoXVREPLI_W:
     Imm |= 0x800;
     break;
   case LoongArch::PseudoVREPLI_D:
+  case LoongArch::PseudoXVREPLI_D:
     Imm |= 0xC00;
     break;
   }
@@ -310,6 +314,11 @@ void LoongArchMCCodeEmitter::encodeInstruction(
   case LoongArch::PseudoVREPLI_W:
   case LoongArch::PseudoVREPLI_D:
     return expandToVectorLDI<LoongArch::VLDI>(MI, OS, Fixups, STI);
+  case LoongArch::PseudoXVREPLI_B:
+  case LoongArch::PseudoXVREPLI_H:
+  case LoongArch::PseudoXVREPLI_W:
+  case LoongArch::PseudoXVREPLI_D:
+    return expandToVectorLDI<LoongArch::XVLDI>(MI, OS, Fixups, STI);
   }
 
   switch (Size) {