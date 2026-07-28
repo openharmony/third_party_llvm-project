26bb21e3b3427759e77b45f0f0216570282b5e24
ziper <louzhipeng@huawei.com>
2026-07-28 03:29:58 +0000

    [lld] Fix CodeSign Merkle slice length
    Original-commit: 03e5f739b85142bf9110a0838545ae2be5570e1c
    Original-author: rePeek <wangsenyin@huawei.com>
    
    Adaptations:
    - Subject corrected from misleading [LLDB] to [lld] (file is lld/ELF/CodeSign.cpp).
    - slice(N,M) length fix applied as-is; MutableArrayRef::slice semantics unchanged on 21.x.
    - No new in-file OHOS_LLVM gates; CodeSign.cpp remains CMake if(OHOS_LLVM) gated.
    
    Change-Id: I862aa79518798966b6704be80495142dd53e9f88
    Signed-off-by: ziper <louzhipeng@huawei.com>

--- lld/ELF/CodeSign.cpp
+++ lld/ELF/CodeSign.cpp
@@ -148,7 +148,8 @@ void CodeSign::generateHashData(ArrayRef<uint8_t> inputBuffer,
     // Generate the buffer portion of upper-level hash data from lower-level
     // hash or original data.
     auto generateHashBuffer =
-        outputBuffer.slice(offsetArrays[i], offsetArrays[i + 1]);
+        outputBuffer.slice(offsetArrays[i],
+                           offsetArrays[i + 1] - offsetArrays[i]);
 
     // Determine the input for this level:
     // - For the bottom level (i == lastIndex), use the original input data
@@ -156,7 +157,8 @@ void CodeSign::generateHashData(ArrayRef<uint8_t> inputBuffer,
     ArrayRef<uint8_t> originalHashBuffer =
         (i == lastIndex)
             ? inputBuffer
-            : outputBuffer.slice(offsetArrays[i + 1], offsetArrays[i + 2]);
+            : outputBuffer.slice(offsetArrays[i + 1],
+                                 offsetArrays[i + 2] - offsetArrays[i + 1]);
 
     size_t size = originalHashBuffer.size();
 
